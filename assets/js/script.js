d22c25b26fddda2b5b38b97e8d15822353b190be
sdpercy <scottpercy@hotmail.com>
2021-05-19 10:06:02 -0230

    added function to color code according to the time of day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 
+
+
+
 //load the html and css files
 $(document).ready(function() {
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
@@ -10,4 +13,29 @@ $(document).ready(function() {
         localStorage.setItem(textInput, timeInput);
     })
 })
+
+
+
+function logHours(){
+    //retrieve the current hour from moment.js
+    var currentTime = moment().hour();
+
+    $(".time-block").each(function(){
+        //split the id string and number
+        var schedulerHours = parseInt($(this).attr("id").split("hr")[1]);
+        //color code hour task description if time is in the past fill grey, if current time fill red, and if
+//future fill green using provided css color classes
+        if (schedulerHours < currentTime){
+            $(this).addClass("past");
+        }
+        else if (schedulerHours === currentTime){
+            $(this).addClass("present");
+        }
+        else {
+            $(this).addClass("future");
+        }
+    })  
+}
+logHours();
+
 
